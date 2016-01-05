c675d501ee0744b3afd0e429fa6ab75248d505e0
ayoubdev <ayoub.adib.dev@gmail.com>
2016-01-05 22:32:15 +0100

    => Update build.gradle in order to add android design support => Update gradle wrapper (bin instead of all) => POC of android design support via TabComponent.java

---
+++ android/src/main/java/fr/ayoubdev/rnak/components/tab/TabComponent.java
@@ -0,0 +1,13 @@
+package fr.ayoubdev.rnak.components.tab;
+
+import android.content.Context;
+import android.support.design.widget.TabLayout;
+
+/**
+ * Created by Adib on 05/01/2016.
+ */
+public class TabComponent extends TabLayout {
+    public TabComponent(Context context) {
+        super(context);
+    }
+}
