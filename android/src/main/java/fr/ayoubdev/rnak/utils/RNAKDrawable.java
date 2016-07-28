3874186bbc903bfef43a2346c4dfb6d5535bda64
Vojtech Novak <vonovak@gmail.com>
2016-07-29 00:29:25 +0200

    return call ignored
    the value from `imageName.replace('-', '_');` is ignored

--- android/src/main/java/fr/ayoubdev/rnak/utils/RNAKDrawable.java
+++ android/src/main/java/fr/ayoubdev/rnak/utils/RNAKDrawable.java
@@ -6,7 +6,7 @@ import android.view.View;
 public class RNAKDrawable {
 	public static <T extends View> int getDrawableID(T view, String filename) {
 		String imageName = getFilenameWithoutExtension(filename);
-		imageName.replace('-', '_');
+		imageName = imageName.replace('-', '_');
 
 		return view.getResources().getIdentifier(imageName, "drawable", view.getContext().getPackageName());
 	}
